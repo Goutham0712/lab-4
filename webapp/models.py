cda7f075f92a1091f248a630aba1618368ce8344
Goutham0712 <patlol03@email.franklin.edu>
2023-12-14 02:26:05 +0300

    Updated front end

--- webapp/models.py
+++ webapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Contacts(models.Model):
+    contact_id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
--- webapp/urls.py
+++ webapp/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'), 
     path('help/', views.help_page, name='help'),
+    path('delete_contact/<int:contact_id>/', views.delete_contact, name='delete_contact'),
+
 ]
